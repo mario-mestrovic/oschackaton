3846235cf51749d2910c199710fea9f912cefe70
ivan varga <ivan.varga.zg@gmail.com>
2015-10-10 23:34:03 +0200

    no message

---
+++ Kiselish/www/app/components/challenge/challengesCtrl - Copy.js
@@ -0,0 +1,8 @@
+﻿angular.module('teglanje').controller('challengesCtrl', ['$scope', 'challenges', function ($scope, challenges) {
+    $scope.Challenges = challenges;
+    
+    $scope.getImage = function (item) {
+        return item.get('photo').url();
+    };
+}]);
+
